b91d036f74ce1fb3f4d7b4c4a600b16a7801e7e2
VladuhaTM <vladis.potap@gmail.com>
2025-02-27 23:17:18 +0300

    commit

---
+++ src/main/java/org/example/Home1.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Home1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 500; i++) {
+            System.out.println(fizzbuzz(i));
+        }
+    }
+    public static String fizzbuzz(int i){
+        if (i % 5 == 0 && i % 7 == 0) {
+            return "fizzbuzz";
+        } else if (i % 5 == 0) {
+            return "fizz";
+        } else if (i % 7 == 0) {
+            return "buzz";
+        } else {
+            return Integer.toString(i);
+        }
+    }
+}
